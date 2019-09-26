1387db6c781b52a2293702fb770702b384b4bf6d
art <art@pop-os.localdomain>
2019-09-26 21:48:44 +0300

    code rewritten using an error handler

--- Bin2Dec.py
+++ Bin2Dec.py
@@ -1,30 +1,20 @@
 print("This program convert binary numbers to decimal.")
 
-num = input("Enter up to 8 binary digits 0 or 1: ")
-
-def converter():
-    bin_num = int(num, 2)
-    print(bin_num)
-    
-    
-    
-def wrong_dig():
-    print("Wrong digits")
-
 def main():
-    if len(num) != 8:
-        wrong_dig()
-        
-    else:
-        for i in num:
-            if i == '0' or i == '1':
-                continue
-            else:
-                wrong_dig()
-                exit()
-        
-        converter()        
+    while True:
+        try:
+            num = input('Enter up to 8 binary digits: ')
+            if len(num) > 8:
+                print("Please, enter up to 8 binary digits")
             
+            else:    
+                bin_num = int(num, 2)
+                print("In Dec: ",bin_num)
+                break
+        
+        except ValueError:
+            print("\nWrong digit!\n")
+        
             
                
 
